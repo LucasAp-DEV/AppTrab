ff34631f4057b30eca1ee38114c48ae2510e6faf
Lucas Silva <lucasapdev@gmail.com>
2023-06-30 19:07:28 -0300

    Add

---
+++ src/components/BotaoPag1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BotaoPag1 = () => {
+
+    return (
+    <div className='botao'>
+      <button onClick={navigateToPage1}>
+        <h2>Pagina 02</h2>
+      </button>
+    </div>
+  );
+};
+
+export default BotaoPag1;
---
+++ src/components/BotaoPag2.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const BotaoPag2 = () => {
+  const navigateToPage2 = () => {
+    // Lógica para navegar para a página 2
+  };
+
+  return (
+    <div className='botao'>
+      <button onClick={navigateToPage2}>
+        <h2>Pagina 02</h2>
+      </button>
+    </div>
+  );
+};
+
+export default BotaoPag2;
